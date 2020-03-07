20af71962cf0c88205b4c667208cd63fdacef971
hans <1078743005@qq.com>
2020-03-07 14:41:15 +0800

    去掉vendor

--- vendor/matyhtf/swoole_framework/tests/array_02.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DEBUG', 'on');
-define('WEBPATH', realpath(__DIR__ . '/..'));
-//包含框架入口文件
-require WEBPATH . '/libs/lib_config.php';
-
-echo _array(["hello:world"])->get(0)->split(':')->get(1);
